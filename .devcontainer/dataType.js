0feefe1b0ae2bb13806f1630739a50970c260a37
Shekhardhami90 <140928408+Shekhardhami90@users.noreply.github.com>
2023-09-09 17:44:35 +0000

    datatype

--- .devcontainer/dataType.js
+++ .devcontainer/dataType.js
@@ -1,1 +1,16 @@
+"use strict";
+// alert(3+3) not runed becouse we are using nodejs , not a browser
+// console.log("hello");
+// console.log("hello2");
 
+// Datatypes
+/*Number
+null
+Boolean
+BigInt
+String
+Symbol
+undefined*/
+
+// Object
+console.log(typeof null);
