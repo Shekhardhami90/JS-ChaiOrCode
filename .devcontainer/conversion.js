61308ddbc26485817e09306de7e97fde2383505d
Shekhardhami90 <140928408+Shekhardhami90@users.noreply.github.com>
2023-09-09 18:08:01 +0000

    conversion

---
+++ .devcontainer/conversion.js
@@ -0,0 +1,12 @@
+let score ="33"
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+let isLoggedIn = 1
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+// boolean ==> 1= true , 0 = false
+// "" ==> false "shekar" ==> true
