3e375a9a2fedfcbc30cffa73b3fab0b069ecbbd5
Shekhardhami90 <140928408+Shekhardhami90@users.noreply.github.com>
2023-09-09 18:12:35 +0000

    conve.

--- .devcontainer/conversion.js
+++ .devcontainer/conversion.js
@@ -10,3 +10,9 @@ console.log(BooleanIsLoggedIn);
 
 // boolean ==> 1= true , 0 = false
 // "" ==> false "shekar" ==> true
+
+// number to String
+let someNumber = 35
+let str = String(someNumber)
+console.log(str);
+console.log(typeof(str));
