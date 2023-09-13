63c5c06628b3f12b1bc40eaa62cfb7cf56991add
Shekhardhami90 <140928408+Shekhardhami90@users.noreply.github.com>
2023-09-13 03:53:19 +0000

    comp

--- .devcontainer/conversion.js
+++ .devcontainer/conversion.js
@@ -1,18 +1,28 @@
-let score = "33";
-console.log(typeof score);
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let score = "33";
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+// let BooleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
-// boolean ==> 1= true , 0 = false
-// "" ==> false "shekar" ==> true
+// // boolean ==> 1= true , 0 = false
+// // "" ==> false "shekar" ==> true
 
-// number to String
-let someNumber = 35;
-let str = String(someNumber);
-console.log(str);
-console.log(typeof str);
+// // number to String
+// let someNumber = 35;
+// let str = String(someNumber);
+// console.log(str);
+// console.log(typeof str);
+
+
+// console.log("1" + 2);
+
+console.log(+ true);
+
+let a = 4;
+const b = ++a;
+console.log((a),(b));
+
