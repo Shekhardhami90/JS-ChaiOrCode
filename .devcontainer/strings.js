f0c5a9cccf85703458bacac3fd14c36fd0fa7e19
Shekhardhami90 <140928408+Shekhardhami90@users.noreply.github.com>
2023-09-16 17:34:03 +0000

    str

---
+++ .devcontainer/strings.js
@@ -0,0 +1,19 @@
+let MyNamen = "shekhar"
+console.log(MyNamen);
+// old method to print strings in js
+
+let name = "Dhami"
+console.log(MyNamen.charAt(3));
+// const rollNumber = "toe zeero"
+// console.log(`name: ${name.toLowerCase()} roll No. ${rollNumber.toUpperCase()}`);
+
+// const gameName = new String('dhami')
+// console.log(gameName.charAT(2));
+
+console.log("hello");
+const gameName = new String('dhami')
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("m"));
+
+// slice method
+const sliceStr = new String("shekhar")
--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,1 +1,4 @@
 console.log("hello");
+const gameName = new String('dhami')
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("m"));
